8f9530234ffa02ffd2b4c398ae2f3fc2e8bf555b
lordofda <lordofda@gmail.com>
2018-02-17 13:54:04 +0100

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -2,6 +2,6 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get rpi-update
-sudo apt-get install wget curl vim dtrx screen htop fortunes build-essential python python3 -y
+sudo apt-get install wget curl vim dtrx screen htop nload fortunes build-essential python python3 -y
 curl https://get.pimoroni.com/uptodate | bash
 //curl -sLS https://apt.adafruit.com/add | sudo bash
