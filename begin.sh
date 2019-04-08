cccd0210282a5c1f39e97af855ec0573bb2cd6df
lordofda <lordofda@gmail.com>
2019-04-08 15:51:53 +0200

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -5,5 +5,7 @@ sudo apt-get rpi-update
 sudo apt-get install wget git curl vim dtrx screen htop nload ncdu fortunes build-essential python python3 -y
 curl https://get.pimoroni.com/uptodate | bash
 //curl -sLS https://apt.adafruit.com/add | sudo bash
-sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add - && echo "deb https://repos.influxdata.com/debian jessie stable" | tee -a /etc/apt/sources.list && sudo apt-get update
+sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+echo "deb https://repos.influxdata.com/debian jessie stable" | tee -a /etc/apt/sources.list
+sudo apt-get update
 sudo apt-get install telegraf -y
