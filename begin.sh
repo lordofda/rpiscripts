4e8e6f07df03b489e313a8e86c092d52f8f12871
lordofda <lordofda@gmail.com>
2019-06-04 15:47:18 +0200

    added inxi

--- begin.sh
+++ begin.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get rpi-update
-sudo apt-get install wget git curl vim dtrx screen htop nload ncdu fortunes build-essential python python3 -y
+sudo apt-get install wget git curl vim dtrx screen htop nload ncdu fortunes build-essential python python3 inxi -y
 curl https://get.pimoroni.com/uptodate | bash
 //curl -sLS https://apt.adafruit.com/add | sudo bash
 sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
