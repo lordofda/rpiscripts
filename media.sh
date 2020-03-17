8ce96f6bcf1094154bf746113d579d36d31be206
lordofda <lordofda@gmail.com>
2020-03-17 09:53:09 +0100

    Update media.sh

--- media.sh
+++ media.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 tree -L 3 -sh -C /home/osmc/Videos | ansi2html.sh > /home/osmc/txt/vid.html
 tree -L 3 -sh -C /home/osmc/Movies | ansi2html.sh > /home/osmc/txt/mov.html
-tree -L 3 -sh -C /home/osmc/Porn | ansi2html.sh > /home/osmc/txt/prn.html
-tree -L 3 -sh -C /media/KINGSTON | ansi2html.sh > /home/osmc/txt/usb.html
 for i in /home/osmc/txt/*; do
 	sed -i '/[0-999]K/d' $i
 	sed -i '/.nfo/d' $i
