3d2543ea9250eb3be9450f060a813aee5fd81c48
lordofda <lordofda@gmail.com>
2020-05-10 18:04:11 +0200

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+#update, upgrade
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get rpi-update
-sudo apt-get install wget git curl vim dtrx screen htop nload ncdu fortunes build-essential python python3 inxi -y
+#rpi already installed
+sudo apt-get install wget git curl htop ncdu python build-essential python3 -y
+#rest
+sudo apt-get install vim dtrx screen nload fortunes inxi -y
 curl https://get.pimoroni.com/uptodate | bash
 //curl -sLS https://apt.adafruit.com/add | sudo bash
+#TICK stack elements
 sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 echo "deb https://repos.influxdata.com/debian jessie stable" | tee -a /etc/apt/sources.list
 sudo apt-get update
