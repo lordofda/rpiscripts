6b00cb882da1aa9062858b4616a7b22dfdce7a81
lordofda <lordofda@gmail.com>
2021-07-19 12:31:24 +0200

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -10,7 +10,7 @@ sudo apt-get install vim screen nload fortunes inxi -y
 sudo apt-get install dtrx -y # could be absent from repo
 cd ~ && git clone https://github/azlux/log2ram.git && cd log2ram && sudo chmod +x ./install.sh && sudo ./install.sh
 curl https://get.pimoroni.com/uptodate | bash
-//curl -sLS https://apt.adafruit.com/add | sudo bash
+#curl -sLS https://apt.adafruit.com/add | sudo bash
 #TICK stack elements
 sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 echo "deb https://repos.influxdata.com/debian buster stable" | sudo tee -a /etc/apt/sources.list
